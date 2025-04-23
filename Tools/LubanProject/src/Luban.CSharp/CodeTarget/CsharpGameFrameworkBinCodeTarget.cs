2b750b0fb3f80d036e05b70b94fbe2e725238461
SunHowe <tosunhowe@gmail.com>
2025-04-23 19:52:54 +0800

    [feat] luban tools

---
+++ Tools/LubanProject/src/Luban.CSharp/CodeTarget/CsharpGameFrameworkBinCodeTarget.cs
@@ -0,0 +1,15 @@
+using Luban.CodeTarget;
+using Luban.CSharp.TemplateExtensions;
+using Scriban;
+
+namespace Luban.CSharp.CodeTarget;
+
+[CodeTarget("cs-gf-bin")]
+public class CsharpGameFrameworkBinCodeTarget : CsharpCodeTargetBase
+{
+    protected override void OnCreateTemplateContext(TemplateContext ctx)
+    {
+        base.OnCreateTemplateContext(ctx);
+        ctx.PushGlobal(new CsharpBinTemplateExtension());
+    }
+}
---
+++ Tools/build-luban.bat
@@ -0,0 +1,5 @@
+rd /s /q Luban
+
+dotnet build  LubanProject/src/Luban/Luban.csproj -c Release -o Luban
+
+pause
