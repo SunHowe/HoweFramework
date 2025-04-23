52810888ae7cd143888ad88dcf652b9151c7bb3d
SunHowe <tosunhowe@gmail.com>
2025-04-23 20:56:13 +0800

    [chore] settingmodule

---
+++ Client/Assets/HoweFramework/Setting/SettingModule.cs
@@ -0,0 +1,20 @@
+namespace HoweFramework
+{
+    /// <summary>
+    /// 设置模块。提供键值对形式的用户设置支持。
+    /// </summary>
+    public sealed class SettingModule : ModuleBase<SettingModule>
+    {
+        protected override void OnInit()
+        {
+        }
+
+        protected override void OnDestroy()
+        {
+        }
+
+        protected override void OnUpdate(float elapseSeconds, float realElapseSeconds)
+        {
+        }
+    }
+}
