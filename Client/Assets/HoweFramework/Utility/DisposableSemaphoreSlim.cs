1a80e5896fb50446495f971e9dc1158fd08d1dcb
SunHowe <tosunhowe@gmail.com>
2025-04-27 20:03:57 +0800

    [feat] global DisposableSemaphoreSlim

--- Client/Assets/HoweFramework/Utility/DisposableSemaphoreSlim.cs
+++ Client/Assets/HoweFramework/Utility/DisposableSemaphoreSlim.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 
@@ -23,6 +24,27 @@ namespace HoweFramework
         public void Dispose()
         {
             m_Semaphore.Release();
+        }
+
+        /// <summary>
+        /// 全局信号量字典。
+        /// </summary>
+        private static readonly ConcurrentDictionary<int, DisposableSemaphoreSlim> s_DisposableSemaphoreSlimMap = new();
+
+        /// <summary>
+        /// 等待全局指定Key的信号量。结合using使用。
+        /// </summary>
+        /// <param name="key">信号量Key。</param>
+        /// <returns>信号量。</returns>
+        public static UniTask<IDisposable> WaitAsync(int key)
+        {
+            if (!s_DisposableSemaphoreSlimMap.TryGetValue(key, out var disposableSemaphoreSlim))
+            {
+                disposableSemaphoreSlim = new DisposableSemaphoreSlim();
+                s_DisposableSemaphoreSlimMap.TryAdd(key, disposableSemaphoreSlim);
+            }
+
+            return disposableSemaphoreSlim.WaitAsync();
         }
     }
 }
