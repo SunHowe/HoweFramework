395187884bb0f7c7917af308ad4b4ac7e8fc0d5a
SunHowe <tosunhowe@gmail.com>
2025-05-27 16:11:54 +0800

    [feat] Login Scene

--- Client/Assets/GameMain/Scripts/Procedure/ProcedureLogin.cs
+++ Client/Assets/GameMain/Scripts/Procedure/ProcedureLogin.cs
@@ -9,10 +9,14 @@ namespace GameMain
     /// </summary>
     public sealed class ProcedureLogin : ProcedureBase
     {
+        private const string LoginSceneAssetName = "Assets/GameMain/Scene/Login.unity";
         public override int Id => (int)ProcedureId.Login;
 
         public override void OnEnter()
         {
+            // 打开登录场景。
+            SceneModule.Instance.LoadSceneAsync(LoginSceneAssetName).Forget();
+
             // 打开登录界面。
             UIModule.Instance.OpenUIForm(UIFormId.LoginForm).Forget();
         }
@@ -21,6 +25,9 @@ namespace GameMain
         {
             // 关闭登录界面。
             UIModule.Instance.CloseUIForm(UIFormId.LoginForm).Forget();
+
+            // 卸载登录场景。
+            SceneModule.Instance.UnloadSceneAsync(LoginSceneAssetName).Forget();
         }
 
         public override void OnUpdate(float elapseSeconds, float realElapseSeconds)
