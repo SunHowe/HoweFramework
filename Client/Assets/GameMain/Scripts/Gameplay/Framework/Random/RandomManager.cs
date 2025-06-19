dec6f439e14339a1ffc78a366b56a36af834c21d
SunHowe <tosunhowe@gmail.com>
2025-06-19 22:30:53 +0800

    [feat] Random Manager
    # Conflicts:
    #	Client/Assets/GameMain/Scripts/Gameplay/GameManagerType.cs

---
+++ Client/Assets/GameMain/Scripts/Gameplay/Framework/Random/RandomManager.cs
@@ -0,0 +1,39 @@
+using System;
+using HoweFramework;
+
+namespace GameMain
+{
+    /// <summary>
+    /// 随机数管理器。
+    /// </summary>
+    public sealed class RandomManager : GameManagerBase, IRandomManager
+    {
+        private Random m_Random;
+
+        public int GetRandom(int min, int max)
+        {
+            m_Random ??= new Random();
+            return m_Random.Next(min, max);
+        }
+
+        public int GetRandom(int max)
+        {
+            m_Random ??= new Random();
+            return m_Random.Next(max);
+        }
+
+        public void SetSeed(int seed)
+        {
+            m_Random = new Random(seed);
+        }
+
+        protected override void OnAwake()
+        {
+        }
+
+        protected override void OnDispose()
+        {
+            m_Random = null;
+        }
+    }
+}
