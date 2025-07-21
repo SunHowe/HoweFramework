42e80eeaac2212ba1cc7e7c3a395f4c5b9d9906c
SunHowe <tosunhowe@gmail.com>
2025-07-21 18:08:01 +0800

    [feat] NumericComponent.Modify

---
+++ Client/Assets/GameMain/Scripts/Gameplay/Common/Numeric/NumericComponentExtensions.cs
@@ -0,0 +1,88 @@
+using System.Collections.Generic;
+
+namespace GameMain
+{
+    /// <summary>
+    /// 属性组件扩展。
+    /// </summary>
+    public static class NumericComponentExtensions
+    {
+        /// <summary>
+        /// 修改属性值。
+        /// </summary>
+        /// <param name="numericComponent">属性组件。</param>
+        /// <param name="modifyDict">修改的属性值。</param>
+        /// <param name="dispatchEvent">是否派发事件。</param>
+        public static void Modify(this NumericComponent numericComponent, Dictionary<int, long> modifyDict, bool dispatchEvent = true)
+        {
+            if (modifyDict == null || modifyDict.Count == 0)
+            {
+                return;
+            }
+
+            foreach (var (id, value) in modifyDict)
+            {
+                numericComponent.Modify(id, value, dispatchEvent);
+            }
+        }
+
+        /// <summary>
+        /// 逆向修改属性值。
+        /// </summary>
+        /// <param name="numericComponent">属性组件。</param>
+        /// <param name="modifyDict">修改的属性值。</param>
+        /// <param name="dispatchEvent">是否派发事件。</param>
+        public static void ModifyReverse(this NumericComponent numericComponent, Dictionary<int, long> modifyDict, bool dispatchEvent = true)
+        {
+            if (modifyDict == null || modifyDict.Count == 0)
+            {
+                return;
+            }
+
+            foreach (var (id, value) in modifyDict)
+            {
+                numericComponent.Modify(id, -value, dispatchEvent);
+            }
+        }
+
+        /// <summary>
+        /// 修改属性值。
+        /// </summary>
+        /// <param name="numericComponent">属性组件。</param>
+        /// <param name="modifyDict">修改的属性值。</param>
+        /// <param name="multiplier">倍数。</param>
+        /// <param name="dispatchEvent">是否派发事件。</param>
+        public static void Modify(this NumericComponent numericComponent, Dictionary<int, long> modifyDict, int multiplier, bool dispatchEvent = true)
+        {
+            if (modifyDict == null || modifyDict.Count == 0 || multiplier == 0)
+            {
+                return;
+            }
+
+            foreach (var (id, value) in modifyDict)
+            {
+                numericComponent.Modify(id, value * multiplier, dispatchEvent);
+            }
+        }
+
+        /// <summary>
+        /// 逆向修改属性值。
+        /// </summary>
+        /// <param name="numericComponent">属性组件。</param>
+        /// <param name="modifyDict">修改的属性值。</param>
+        /// <param name="multiplier">倍数。</param>
+        /// <param name="dispatchEvent">是否派发事件。</param>
+        public static void ModifyReverse(this NumericComponent numericComponent, Dictionary<int, long> modifyDict, int multiplier, bool dispatchEvent = true)
+        {
+            if (modifyDict == null || modifyDict.Count == 0 || multiplier == 0)
+            {
+                return;
+            }
+
+            foreach (var (id, value) in modifyDict)
+            {
+                numericComponent.Modify(id, -value * multiplier, dispatchEvent);
+            }
+        }
+    }
+}
