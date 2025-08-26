b9213cd26c87f58cc4ea57f9972c9b40fed62b31
SunHowe <tosunhowe@gmail.com>
2025-08-26 11:28:42 +0800

    [feat] game procedure

--- Client/Assets/GameMain/Scripts/Procedure/ProcedureGame.cs
+++ Client/Assets/GameMain/Scripts/Procedure/ProcedureGame.cs
@@ -9,16 +9,34 @@ namespace GameMain
     {
         public override int Id => (int)ProcedureId.Game;
 
+        private IGameContext m_GameContext;
+        private IGameUpdateManager m_GameUpdateManager;
+
         protected override void OnEnter()
         {
+            // 取出游戏上下文。
+            m_GameContext = IOCModule.Instance.Get<IGameContext>();
+            m_GameContext.Awake();
+
+            m_GameUpdateManager = m_GameContext.GetManager<IGameUpdateManager>();
+
+            // 单机游戏在Awake后可以立即StartGame。
+            m_GameContext.StartGame();
         }
 
         protected override void OnLeave()
         {
+            // 清理游戏上下文。
+            IOCModule.Instance.UnRegister(m_GameContext);
+            
+            m_GameContext.Dispose();
+            m_GameContext = null;
+            m_GameUpdateManager = null;
         }
 
         protected override void OnUpdate(float elapseSeconds, float realElapseSeconds)
         {
+            m_GameUpdateManager?.Update(elapseSeconds);
         }
     }
 }
