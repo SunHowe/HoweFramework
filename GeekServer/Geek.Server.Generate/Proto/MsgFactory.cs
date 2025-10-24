d0206d8c6ebb4d12c2a6823993c47eb49d4a38a3
SunHowe <tosunhowe@gmail.com>
2025-10-24 09:54:48 +0800

    [feat] 生成的MsgFactory支持反向查询协议id

--- GeekServer/Geek.Server.Generate/Proto/MsgFactory.cs
+++ GeekServer/Geek.Server.Generate/Proto/MsgFactory.cs
@@ -6,6 +6,7 @@ namespace Geek.Server.Proto
 	public class MsgFactory
 	{
 		private static readonly System.Collections.Generic.Dictionary<int, Type> lookup;
+		private static readonly System.Collections.Generic.Dictionary<Type, int> revertLookup;
 
         static MsgFactory()
         {
@@ -31,6 +32,29 @@ namespace Geek.Server.Proto
 			    { 1179199001, typeof(Geek.Server.Proto.ResErrorCode) },
 			    { 537499886, typeof(Geek.Server.Proto.ResPrompt) },
             };
+			
+            revertLookup = new System.Collections.Generic.Dictionary<Type, int>(19)
+            {
+			    { typeof(Geek.Server.Proto.ReqBagInfo), 1435193915 },
+			    { typeof(Geek.Server.Proto.ResBagInfo), -1872884227 },
+			    { typeof(Geek.Server.Proto.ReqComposePet), 225320501 },
+			    { typeof(Geek.Server.Proto.ResComposePet), 750865816 },
+			    { typeof(Geek.Server.Proto.ReqUseItem), 1686846581 },
+			    { typeof(Geek.Server.Proto.ReqSellItem), -1395845865 },
+			    { typeof(Geek.Server.Proto.ResItemChange), 901279609 },
+			    { typeof(ClientProto.NetConnectMessage), 667869091 },
+			    { typeof(ClientProto.NetDisConnectMessage), 1245418514 },
+			    { typeof(Geek.Server.Proto.TestStruct), 299119425 },
+			    { typeof(Geek.Server.Proto.A), 1250601847 },
+			    { typeof(Geek.Server.Proto.B), -899515946 },
+			    { typeof(Geek.Server.Proto.UserInfo), -593677237 },
+			    { typeof(Geek.Server.Proto.ReqLogin), 1267074761 },
+			    { typeof(Geek.Server.Proto.ResLogin), 785960738 },
+			    { typeof(Geek.Server.Proto.ResLevelUp), 1587576546 },
+			    { typeof(Geek.Server.Proto.HearBeat), 1575482382 },
+			    { typeof(Geek.Server.Proto.ResErrorCode), 1179199001 },
+			    { typeof(Geek.Server.Proto.ResPrompt), 537499886 },
+            };
         }
 
         public static Type GetType(int msgId)
@@ -41,5 +65,12 @@ namespace Geek.Server.Proto
 				throw new Exception($"can not find msg type :{msgId}");
 		}
 
+		public static int GetMsgId(Type type)
+		{
+			if (revertLookup.TryGetValue(type, out int msgId))
+				return msgId;
+			else
+				throw new Exception($"can not find msg id :{type.FullName}");
+		}
 	}
 }
